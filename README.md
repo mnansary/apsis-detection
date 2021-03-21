590b2b68afe8e167e1e45cb6b649e4f8ca8692ef
silentassasin0111 <silentassasin0111@gmail.com>
2021-03-21 16:51:11 +0600

    Background Selection

--- README.md
+++ README.md
@@ -23,6 +23,24 @@ Gnome       : 3.28.2
 * ```pip3 install pillow --global-option="build_ext" --global-option="--enable-freetype"```
 * ```pip3 install -r requirements.txt``` 
 > Its better to use a virtual environment 
+# Dataset
+* **caution**: Do not change anything under **resources**. The directory should look as follows
+```python
+    ├── graphemes.csv
+    └── numbers.csv
+```
+* The **grapheme** dataset is taken from [here](https://www.kaggle.com/pestipeti/bengali-quick-eda/#data). 
+    * Only the **256** folder under **256_train** is kept and renamed as **RAW** form **BengaliAI:Supplementary dataset for BengaliAI Competition**
+* The **number** dataset is taken from [here](https://www.kaggle.com/nazmuddhohaansary/banglasymbols) 
+    * Only the **RAW_NUMS** folder is kept that contains all the images of the numbers
+* The final **data** folder is like as follows:
+
+```python
+    data
+    ├── RAW_NUMS
+    └── RAW
+```
+
 
 # ISSUES FACED
 * **UpsampleLike** layer is tf-1 compat : https://github.com/tensorflow/tensorflow/issues/45207
---
+++ data.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+# -*-coding: utf-8 -
+'''
+    @author:  MD. Nazmuddoha Ansary
+'''
+#--------------------
+# imports
+#--------------------
+import argparse
+import os 
+import json
+import pandas as pd
+from tqdm import tqdm
+import sys 
+
+#--------------------
+# main
+#--------------------
+def main(args):
+    '''
+    '''  
+    data_path   =   args.data_path
+    save_path   =   args.save_path
+    data_dim    =   int(args.data_dim)
+    num_data    =   int(args.num_data)
+    # ops
+    raw_path        = os.path.join(data_path,'RAW')
+    raw_nums_path   = os.path.join(data_path,'RAW_NUMS')
+    # error check
+    LOG_INFO("checking args error")
+    if not os.path.exists(raw_path):
+        raise ValueError("Wrong Data directory given. No RAW png folder in data path")
+    if not os.path.exists(raw_nums_path):
+        raise ValueError("Wrong Data directory given. No RAW_NUMS png folder in data path")
+                        
+    
+               
+    
+#-----------------------------------------------------------------------------------
+
+if __name__=="__main__":
+    '''
+        parsing and execution
+    '''
+    parser = argparse.ArgumentParser("Recognizer Training Dataset Creating Script")
+    parser.add_argument("data_path", help="Path of the data folder that contains converted and raw folder from ReadMe.md)")
+    parser.add_argument("save_path", help="Path of the directory to save the dataset")
+    parser.add_argument("--data_dim",required=False,default=512,help ="final size of each image: default=512")
+    parser.add_argument("--num_data",required=False,default=100000,help ="number of image data to create : default=100000")
+    args = parser.parse_args()
+    main(args)
+    
+    
