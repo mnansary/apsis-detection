e9e37c03111bb8dd71c8121ba31e24ffa74198ec
silentassasin0111 <silentassasin0111@gmail.com>
2021-06-21 23:11:20 +0600

    Detection Dataset

--- coreLib/format.py
+++ coreLib/format.py
@@ -7,11 +7,12 @@
 #--------------------
 import cv2
 import numpy as np
+
 #--------------------
 # format
 #--------------------
 
-def convertToTotalText(page,labels,orientation = 'h'):
+def convertToTotalText(page,labels):
     '''
         **_label_mask:polygon
         ** text_lines[0]=
@@ -30,41 +31,40 @@ def convertToTotalText(page,labels,orientation = 'h'):
         FUTURE:
             * Rotation will be added after render class 
     '''
-    # your code starts from here 
-    # after finalization change returns segment under doc string above
-    
-    # char mask
-    char_mask=np.zeros(page.shape)
-    for line_labels in labels:
-        for label in line_labels:
-            for k,v in label.items():
-                if v!=' ':
-                    char_mask[page==k]=255
-
-    char_mask=np.expand_dims(char_mask,axis=-1)
-    char_mask=np.concatenate([char_mask,char_mask,char_mask],axis=-1)
-    char_mask=char_mask.astype("uint8")
-    
-    
-    
     # text lines
     text_lines=[]
     # word_mask
     word_mask=np.zeros(page.shape)
-    
+    # char mask
+    char_mask=np.zeros(page.shape)
     
     for line_labels in labels:
         for label in line_labels:
             _label_mask=np.zeros((page.shape), dtype=np.uint8)
             transcriptions=''
+            _ymins=[]
+            _ymaxs=[]
+            _xmins=[]
+            _xmaxs=[] 
+            
+            
             for k,v in label.items():
                 if v!=' ':
+                    char_mask[page==k]=255
+                    
                     transcriptions+=v
                     idx = np.where(page==k)
+                    
                     y_min,y_max,x_min,x_max = np.min(idx[0]), np.max(idx[0]), np.min(idx[1]), np.max(idx[1])
-                    word_mask[y_min:y_max+1,x_min:x_max+1]=255
-                    _label_mask[y_min:y_max+1,x_min:x_max+1]=255
+                    _ymins.append(y_min)
+                    _ymaxs.append(y_max)
+                    _xmins.append(x_min)
+                    _xmaxs.append(x_max)            
+                    
             
+            
+            _label_mask[min(_ymins):max(_ymaxs)+1,min(_xmins):max(_xmaxs)+1]=255
+            word_mask[min(_ymins):max(_ymaxs)+1,min(_xmins):max(_xmaxs)+1]=255
             
             contours, hiearchy = cv2.findContours(_label_mask, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
             
@@ -79,11 +79,15 @@ def convertToTotalText(page,labels,orientation = 'h'):
             
             X.append(x_cord)
             Y.append(y_cord)
-        
-            
-            coordinates = "x: " + str(np.array(X)) + ", " + "y: " + str(np.array(Y)) + ", "+"ornt: "+ str([orientation]) + ", "+ "transcriptions: " + str([transcriptions])  
+            orientation="u'h'"
+            coordinates = "x: " + str(np.array(X)) + ", " + "y: " + str(np.array(Y)) + ", "+"ornt: "+ "["+orientation +"]"+", "+ "transcriptions: " + f"[u'{transcriptions}']"  
             text_lines.append(coordinates)
             
+            
+    char_mask=np.expand_dims(char_mask,axis=-1)
+    char_mask=np.concatenate([char_mask,char_mask,char_mask],axis=-1)
+    char_mask=char_mask.astype("uint8")
+    
     word_mask=np.expand_dims(word_mask,axis=-1)
     word_mask=np.concatenate([word_mask,word_mask,word_mask],axis=-1)
     word_mask=word_mask.astype("uint8")
