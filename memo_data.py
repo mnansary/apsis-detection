0ccf31b407cc1626a9073c0c42afd60c39073b44
silentassasin0111 <silentassasin0111@gmail.com>
2021-08-10 02:06:02 +0600

    ServerTest

---
+++ memo_data.py
@@ -0,0 +1,36 @@
+from memoLib.dataset import DataSet
+from memoLib.utils import create_dir,LOG_INFO
+from memoLib.joiner import create_memo_data
+
+data_dir= "/media/ansary/DriveData/Work/bengalAI/datasets/Detection/source"
+save_dir="/media/ansary/DriveData/Work/bengalAI/datasets/Detection/"
+save_dir=create_dir(save_dir,"memo_sep")
+img_dir =create_dir(save_dir,"images")
+pr_dir =create_dir(save_dir,"prints")
+hw_dir =create_dir(save_dir,"hands")
+n_data=10
+ds=DataSet(data_dir)
+
+
+
+from tqdm.auto import tqdm
+import os
+import cv2
+import random
+dim=(512,512)
+for i in tqdm(range(n_data)):
+    try:
+        lang=random.choice(["bangla","english"])
+        img,pr,hw=create_memo_data(ds,lang)
+        img=cv2.resize(img,dim)
+        pr=cv2.resize(pr,dim,fx=0,fy=0, interpolation = cv2.INTER_NEAREST)
+        hw=cv2.resize(hw,dim,fx=0,fy=0, interpolation = cv2.INTER_NEAREST)
+        # save
+        cv2.imwrite(os.path.join(pr_dir,f"{i}.png"),pr)
+        cv2.imwrite(os.path.join(hw_dir,f"{i}.png"),hw)
+        img = cv2.medianBlur(img,5)
+        cv2.imwrite(os.path.join(img_dir,f"{i}.png"),img)
+        
+    except Exception as e:
+        LOG_INFO(f"Skipping:{i}")
+        print(e)
