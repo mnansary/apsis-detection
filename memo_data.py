aa94d7706abc8d6bb0ad82fa5d20e352d5d4408c
silentassasin0111 <silentassasin0111@gmail.com>
2021-08-11 20:24:41 +0600

    Base Table

--- memo_data.py
+++ memo_data.py
@@ -1,15 +1,16 @@
 from memoLib.dataset import DataSet
 from memoLib.utils import create_dir,LOG_INFO
-from memoLib.joiner import create_memo_data
+from memoLib.joiner import create_table_data
 
 data_dir= "/home/apsisdev/ansary/DATASETS/Detection/source/"
 save_dir="/home/apsisdev/ansary/DATASETS/Detection/"
-save_dir=create_dir(save_dir,"memo_segs")
+save_dir=create_dir(save_dir,"memo_table")
+save_dir=create_dir(save_dir,"base")
 img_dir =create_dir(save_dir,"images")
-pr_dir =create_dir(save_dir,"data")
-#hw_dir =create_dir(save_dir,"hands")
-tb_dir =create_dir(save_dir,"table")
-n_data=10000
+tmap_dir =create_dir(save_dir,"data")
+wmap_dir =create_dir(save_dir,"hands")
+cmap_dir =create_dir(save_dir,"table")
+n_data=1000
 ds=DataSet(data_dir)
 
 
@@ -18,21 +19,22 @@ from tqdm.auto import tqdm
 import os
 import cv2
 import random
-dim=(512,512)
+dim=(256,256)
 for i in tqdm(range(n_data)):
     try:
         lang=random.choice(["bangla","english"])
-        img,pr,tb=create_memo_data(ds,lang)
+        img,tmap,cmap,wmap=create_table_data(ds,lang)
         img=cv2.resize(img,dim)
-        pr=cv2.resize(pr,dim,fx=0,fy=0, interpolation = cv2.INTER_NEAREST)
-        #hw=cv2.resize(hw,dim,fx=0,fy=0, interpolation = cv2.INTER_NEAREST)
-        tb=cv2.resize(tb,dim,fx=0,fy=0, interpolation = cv2.INTER_NEAREST)
+        tmap=cv2.resize(tmap,dim,fx=0,fy=0, interpolation = cv2.INTER_NEAREST)
+        cmap=cv2.resize(cmap,dim,fx=0,fy=0, interpolation = cv2.INTER_NEAREST)
+        wmap=cv2.resize(wmap,dim,fx=0,fy=0, interpolation = cv2.INTER_NEAREST)
         # save
-        cv2.imwrite(os.path.join(tb_dir,f"{i}.png"),tb)
-        cv2.imwrite(os.path.join(pr_dir,f"{i}.png"),pr)
-        #cv2.imwrite(os.path.join(hw_dir,f"{i}.png"),hw)
+        cv2.imwrite(os.path.join(tmap_dir,f"{i}.png"),tmap)
+        cv2.imwrite(os.path.join(cmap_dir,f"{i}.png"),cmap)
+        cv2.imwrite(os.path.join(wmap,f"{i}.png"),wmap)
+        
         # ksize
-        ksize = (5, 5)        
+        ksize = (3, 3)        
         # Using cv2.blur() method 
         img = cv2.blur(img, ksize)
         cv2.imwrite(os.path.join(img_dir,f"{i}.png"),img)
