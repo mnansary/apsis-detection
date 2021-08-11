c1b267faf6f3fbc50a45638ac705cc73fd9d37e2
silentassasin0111 <silentassasin0111@gmail.com>
2021-08-11 20:34:24 +0600

    Base Table

--- memo_data.py
+++ memo_data.py
@@ -6,10 +6,10 @@ data_dir= "/home/apsisdev/ansary/DATASETS/Detection/source/"
 save_dir="/home/apsisdev/ansary/DATASETS/Detection/"
 save_dir=create_dir(save_dir,"memo_table")
 save_dir=create_dir(save_dir,"base")
-img_dir =create_dir(save_dir,"images")
-tmap_dir =create_dir(save_dir,"data")
-wmap_dir =create_dir(save_dir,"hands")
-cmap_dir =create_dir(save_dir,"table")
+img_dir =create_dir(save_dir,"image")
+tmap_dir =create_dir(save_dir,"textmap")
+wmap_dir =create_dir(save_dir,"wordmap")
+cmap_dir =create_dir(save_dir,"charmap")
 n_data=1000
 ds=DataSet(data_dir)
 
