716c311aa048b53b47abda17d5fa349860d61b86
rezwanh001 <rezwanh001@gmail.com>
2021-09-08 14:19:11 +0600

    Corrected Heatmap ratio in server.sh

--- scripts/server.sh
+++ scripts/server.sh
@@ -14,7 +14,7 @@ python store.py "${PROCESSED_PATH}memo/images/" $DATASET_MAIN_PATH memo
 python boise_state.py $BS_README_TXT_PATH $PROCESSED_PATH
 python store.py "${PROCESSED_PATH}bs/images/" $DATASET_MAIN_PATH bs 
 # synthetic
-python synthetic.py $BASE_DATA_PATH $PROCESSED_PATH linetext synth --cfg_heatmap_ratio 4
+python synthetic.py $BASE_DATA_PATH $PROCESSED_PATH linetext synth 
 python store.py "${PROCESSED_PATH}synth.train/images/" $DATASET_MAIN_PATH synth.train 
 python store.py "${PROCESSED_PATH}synth.test/images/" $DATASET_MAIN_PATH synth.test 
 echo succeded
